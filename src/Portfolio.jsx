984f3be8d5e05f814dfcc38b0402d8b20a5bab9d
Bogdan Balikhin <bbalikhin@nuvem.com>
2025-06-12 19:46:08 +0300

    Support for %PUBLIC_URL% in markdown files

--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -6,13 +6,22 @@ export default function Portfolio() {
 
   useEffect(() => {
     fetch(`${process.env.PUBLIC_URL}/content/portfolio.md`)
-      .then(res => res.text())
+      .then((res) => res.text())
       .then(setContent);
   }, []);
 
   return (
     <main className="max-w-4xl mx-auto p-6 font-sans text-gray-900 prose prose-lg">
-      <ReactMarkdown>{content}</ReactMarkdown>
+      <ReactMarkdown
+        components={{
+          a: ({ node, ...props }) => {
+            const href = props.href?.replace('%PUBLIC_URL%', process.env.PUBLIC_URL);
+            return <a {...props} href={href} />;
+          },
+        }}
+      >
+        {content}
+      </ReactMarkdown>
     </main>
   );
 }
